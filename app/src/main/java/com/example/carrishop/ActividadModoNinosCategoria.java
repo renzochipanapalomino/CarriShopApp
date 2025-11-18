d18102951ec0b57ac9a33c564a6090e2b00f2968
renzochipana <142509397+renzochipana@users.noreply.github.com>
2025-11-17 23:57:51 -0500

    Subo todos los archivos del proyecto CarriShop

--- app/src/main/java/com/example/carrishop/ActividadModoNinosCategoria.java
+++ app/src/main/java/com/example/carrishop/ActividadModoNinosCategoria.java
@@ -143,12 +143,12 @@ public class ActividadModoNinosCategoria extends AppCompatActivity implements Ki
             case COLORS:
                 items.add(new KidsCategoryItem("color_rojo", getString(R.string.kids_color_red), "", red, "color_rojo"));
                 items.add(new KidsCategoryItem("color_azul", getString(R.string.kids_color_blue), "", blue, "color_azul"));
-                items.add(new KidsCategoryItem("color_amarillo", getString(R.string.kids_color_yellow), "", yellow, "color_amarillo"));
+                items.add(new KidsCategoryItem("color_yellow", getString(R.string.kids_color_yellow), "", yellow, "color_yellow"));
                 items.add(new KidsCategoryItem("color_verde", getString(R.string.kids_color_green), "", green, "color_verde"));
                 items.add(new KidsCategoryItem("color_naranja", getString(R.string.kids_color_orange), "", orange, "color_naranja"));
-                items.add(new KidsCategoryItem("color_morado", getString(R.string.kids_color_purple), "", purple, "color_morado"));
-                items.add(new KidsCategoryItem("color_rosa", getString(R.string.kids_color_pink), "", pink, "color_rosa"));
-                items.add(new KidsCategoryItem("color_blanco", getString(R.string.kids_color_white), "", ContextCompat.getColor(this, R.color.white), "color_blanco"));
+                items.add(new KidsCategoryItem("color_marron", getString(R.string.kids_color_purple), "", purple, "color_marron"));
+                items.add(new KidsCategoryItem("color_purpura", getString(R.string.kids_color_pink), "", pink, "color_purpura"));
+                items.add(new KidsCategoryItem("color_rosado", getString(R.string.kids_color_white), "", ContextCompat.getColor(this, R.color.white), "color_rosado"));
                 items.add(new KidsCategoryItem("color_negro", getString(R.string.kids_color_black), "", ContextCompat.getColor(this, R.color.black), "color_negro"));
                 break;
             case NUMBERS:
@@ -163,25 +163,25 @@ public class ActividadModoNinosCategoria extends AppCompatActivity implements Ki
                 }
                 break;
             case FRUITS:
-                items.add(new KidsCategoryItem("fruta_manzana", getString(R.string.kids_fruit_apple), "🍎", red, "frutas_manzana"));
-                items.add(new KidsCategoryItem("fruta_platano", getString(R.string.kids_fruit_banana), "🍌", yellow, "frutas_platano"));
-                items.add(new KidsCategoryItem("fruta_naranja", getString(R.string.kids_fruit_orange), "🍊", orange, "frutas_naranja"));
-                items.add(new KidsCategoryItem("fruta_uvas", getString(R.string.kids_fruit_grape), "🍇", purple, "frutas_uvas"));
-                items.add(new KidsCategoryItem("fruta_sandia", getString(R.string.kids_fruit_watermelon), "🍉", green, "frutas_sandia"));
+                items.add(new KidsCategoryItem("frutas", getString(R.string.kids_fruit_apple), "🍎", red, "frutas"));
+                items.add(new KidsCategoryItem("frutas", getString(R.string.kids_fruit_banana), "🍌", yellow, "frutas"));
+                items.add(new KidsCategoryItem("frutas", getString(R.string.kids_fruit_orange), "🍊", orange, "frutas"));
+                items.add(new KidsCategoryItem("frutas", getString(R.string.kids_fruit_grape), "🍇", purple, "frutas"));
+                items.add(new KidsCategoryItem("frutas", getString(R.string.kids_fruit_watermelon), "🍉", green, "frutas"));
                 break;
             case VOWELS:
-                items.add(new KidsCategoryItem("vocal_a", getString(R.string.kids_vowel_a), "A", pink, "vocales"));
-                items.add(new KidsCategoryItem("vocal_e", getString(R.string.kids_vowel_e), "E", orange, "vocales"));
-                items.add(new KidsCategoryItem("vocal_i", getString(R.string.kids_vowel_i), "I", blue, "vocales"));
-                items.add(new KidsCategoryItem("vocal_o", getString(R.string.kids_vowel_o), "O", green, "vocales"));
-                items.add(new KidsCategoryItem("vocal_u", getString(R.string.kids_vowel_u), "U", purple, "vocales"));
+                items.add(new KidsCategoryItem("vocales", getString(R.string.kids_vowel_a), "A", pink, "vocales"));
+                items.add(new KidsCategoryItem("vocales", getString(R.string.kids_vowel_e), "E", orange, "vocales"));
+                items.add(new KidsCategoryItem("vocales", getString(R.string.kids_vowel_i), "I", blue, "vocales"));
+                items.add(new KidsCategoryItem("vocales", getString(R.string.kids_vowel_o), "O", green, "vocales"));
+                items.add(new KidsCategoryItem("vocales", getString(R.string.kids_vowel_u), "U", purple, "vocales"));
                 break;
             case ANIMALS:
             default:
-                items.add(new KidsCategoryItem("animal_leon", getString(R.string.kids_animal_lion), "🦁", orange, "animal_leon"));
-                items.add(new KidsCategoryItem("animal_elefante", getString(R.string.kids_animal_elephant), "🐘", teal, "animal_elefante"));
-                items.add(new KidsCategoryItem("animal_mono", getString(R.string.kids_animal_monkey), "🐵", brown, "animal_mono"));
-                items.add(new KidsCategoryItem("animal_rana", getString(R.string.kids_animal_frog), "🐸", green, "animal_rana"));
+                items.add(new KidsCategoryItem("animal_perro", getString(R.string.kids_animal_lion), "🦁", orange, "animal_perro"));
+                items.add(new KidsCategoryItem("animal_gallo", getString(R.string.kids_animal_elephant), "🐘", teal, "animal_gallo"));
+                items.add(new KidsCategoryItem("animal_gato", getString(R.string.kids_animal_monkey), "🐵", brown, "animal_gato"));
+                items.add(new KidsCategoryItem("animal_chivo", getString(R.string.kids_animal_frog), "🐸", green, "animal_chivo"));
                 items.add(new KidsCategoryItem("animal_ave", getString(R.string.kids_animal_bird), "🕊️", blue, "animal_ave"));
                 break;
         }
